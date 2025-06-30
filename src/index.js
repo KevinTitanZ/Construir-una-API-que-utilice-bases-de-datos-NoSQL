249d82732dcc60809fb7f4acc46648ebeac2c9b8
Kevin Lenin Ordoñez Cabrera <138833988+KevinTitanZ@users.noreply.github.com>
2025-06-30 08:34:14 -0500

    Laboratorio 2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
+
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 // MongoDB
 const connectDB = require('./db');
 connectDB(); // Solo si usas MongoDB
 
+
 // Middleware
+app.use(cors());
 app.use(express.json());
 
 // Rutas
